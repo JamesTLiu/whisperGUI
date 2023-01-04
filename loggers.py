2a1fad212e9e6c9b7cd76960c016ee1b92fe0472
JamesTLiu <21352182+JamesTLiu@users.noreply.github.com>
2023-01-04 11:22:51 -0700

    added file with shared logger

---
+++ loggers.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# mypy: disable-error-code=union-attr
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+f_handler = logging.FileHandler("log.txt")
+f_handler.setLevel(logging.WARNING)
+
+log_format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+
+f_format = logging.Formatter(log_format)
+
+f_handler.setFormatter(f_format)
+
+logger.addHandler(f_handler)
